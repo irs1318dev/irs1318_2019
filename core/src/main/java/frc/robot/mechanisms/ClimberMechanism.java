b3b93b49a0d6bb52b688b2c167ce21fa1bf5caff
ManasiShah1 <manasi.shah@comcast.net>
2019-01-31 19:29:37 -0800

    limit switch variables

--- core/src/main/java/frc/robot/mechanisms/ClimberMechanism.java
+++ core/src/main/java/frc/robot/mechanisms/ClimberMechanism.java
@@ -11,6 +11,7 @@ import frc.robot.common.IMechanism;
 import frc.robot.common.robotprovider.ITalonSRX;
 import frc.robot.common.robotprovider.TalonSRXControlMode;
 import frc.robot.common.robotprovider.TalonSRXFeedbackDevice;
+import frc.robot.common.robotprovider.TalonSRXLimitSwitchStatus;
 import frc.robot.common.robotprovider.ITimer;
 import frc.robot.common.robotprovider.IDashboardLogger;
 import frc.robot.driver.common.Driver;
@@ -35,10 +36,14 @@ public class ClimberMechanism implements IMechanism
     private double armsVelocity;
     private double armsError;
     private int armsPosition;
+    private boolean armsForwardLimitSwitchStatus;
+    private boolean armsReverseLimitSwitchStatus;
 
     private double camVelocity;
     private double camError;
     private int camPosition;
+    private boolean camForwardLimitSwitchStatus;
+    private boolean camReverseLimitSwitchStatus;
 
     private double desiredArmsPosition;
     private double desiredCamPosition;
@@ -63,6 +68,12 @@ public class ClimberMechanism implements IMechanism
             ClimberMechanism.pidSlotId);
         this.climberArmsMotorMaster.setPosition(
             (int)(TuningConstants.CLIMBER_ARMS_RETRACTED_POSITION / HardwareConstants.CLIMBER_ARMS_PULSE_DISTANCE));
+        this.climberArmsMotorMaster.setForwardLimitSwitch(
+            TuningConstants.CLIMBER_ARMS_FORWARD_LIMIT_SWITCH_ENABLED,
+            TuningConstants.CLIMBER_ARMS_FORWARD_LIMIT_SWITCH_NORMALLY_OPEN);
+        this.climberArmsMotorMaster.setReverseLimitSwitch(
+            TuningConstants.CLIMBER_ARMS_REVERSE_LIMIT_SWITCH_ENABLED,
+            TuningConstants.CLIMBER_ARMS_REVERSE_LIMIT_SWITCH_NORMALLY_OPEN);
 
         this.climberArmsMotorMaster.setControlMode(TalonSRXControlMode.Position);
         this.climberArmsMotorMaster.setSelectedSlot(ClimberMechanism.pidSlotId);    
@@ -76,6 +87,8 @@ public class ClimberMechanism implements IMechanism
         this.armsVelocity = 0.0;
         this.armsError = 0.0;
         this.armsPosition = 0;
+        this.armsForwardLimitSwitchStatus = false;
+        this.armsReverseLimitSwitchStatus = false;
         
         this.climberCamMotorMaster = provider.getTalonSRX(ElectronicsConstants.CLIMBER_CAM_MOTOR_MASTER_ID);
         this.climberCamMotorMaster.setNeutralMode(TalonSRXNeutralMode.Brake);
@@ -88,9 +101,14 @@ public class ClimberMechanism implements IMechanism
             TuningConstants.CLIMBER_CAM_POSITION_PID_KD,
             TuningConstants.CLIMBER_CAM_POSITION_PID_KF,
             ClimberMechanism.pidSlotId);
-
         this.climberCamMotorMaster.setPosition(
             (int)(TuningConstants.CLIMBER_CAM_STORED_POSITION / HardwareConstants.CLIMBER_CAM_PULSE_DISTANCE));
+        this.climberCamMotorMaster.setForwardLimitSwitch(
+            TuningConstants.CLIMBER_CAM_FORWARD_LIMIT_SWITCH_ENABLED,
+            TuningConstants.CLIMBER_CAM_FORWARD_LIMIT_SWITCH_NORMALLY_OPEN);
+        this.climberCamMotorMaster.setReverseLimitSwitch(
+            TuningConstants.CLIMBER_CAM_REVERSE_LIMIT_SWITCH_ENABLED,
+            TuningConstants.CLIMBER_CAM_REVERSE_LIMIT_SWITCH_NORMALLY_OPEN);
 
         this.climberCamMotorMaster.setControlMode(TalonSRXControlMode.Position);
         this.climberCamMotorMaster.setSelectedSlot(ClimberMechanism.pidSlotId);    
@@ -104,6 +122,8 @@ public class ClimberMechanism implements IMechanism
         this.camVelocity = 0.0;
         this.camError = 0.0;
         this.camPosition = 0;
+        this.camForwardLimitSwitchStatus = false;
+        this.camReverseLimitSwitchStatus = false;
     }
 
     public double getArmsVelocity() 
@@ -121,6 +141,16 @@ public class ClimberMechanism implements IMechanism
         return this.armsPosition;
     }
 
+    public boolean getArmsForwardLimitSwitchStatus()
+    {
+        return this.armsForwardLimitSwitchStatus;
+    }
+
+    public boolean getArmsReverseLimitSwitchStatus()
+    {
+        return this.armsReverseLimitSwitchStatus;
+    }
+
     public double getCamVelocity() 
     {
         return this.camVelocity;
@@ -136,6 +166,16 @@ public class ClimberMechanism implements IMechanism
         return this.camPosition;
     }
 
+    public boolean getCamForwardLimitSwitchStatus()
+    {
+        return this.camForwardLimitSwitchStatus;
+    }
+
+    public boolean getCamReverseLimitSwitchStatus()
+    {
+        return this.camReverseLimitSwitchStatus;
+    }
+
     @Override
     public void setDriver(Driver driver)
     {
@@ -149,17 +189,30 @@ public class ClimberMechanism implements IMechanism
         this.armsError = this.climberArmsMotorMaster.getError();
         this.armsPosition = this.climberArmsMotorMaster.getPosition();
 
+        TalonSRXLimitSwitchStatus armsLimitSwitchStatus = this.climberArmsMotorMaster.getLimitSwitchStatus();
+        this.armsForwardLimitSwitchStatus = armsLimitSwitchStatus.isForwardClosed;
+        this.armsReverseLimitSwitchStatus = armsLimitSwitchStatus.isReverseClosed;
+
         this.camVelocity = this.climberCamMotorMaster.getVelocity();
         this.camError = this.climberCamMotorMaster.getError();
         this.camPosition = this.climberCamMotorMaster.getPosition();
 
+        TalonSRXLimitSwitchStatus camLimitSwitchStatus = this.climberCamMotorMaster.getLimitSwitchStatus();
+        this.camForwardLimitSwitchStatus = camLimitSwitchStatus.isForwardClosed;
+        this.camReverseLimitSwitchStatus = camLimitSwitchStatus.isReverseClosed;
+
+
         this.logger.logNumber(ClimberMechanism.logName, "armsVelocity", this.armsVelocity);
         this.logger.logNumber(ClimberMechanism.logName, "armsError", this.armsError);
         this.logger.logNumber(ClimberMechanism.logName, "armsPosition", this.armsPosition);
+        this.logger.logBoolean(ClimberMechanism.logName, "armsForwardLimitSwitchStatus", this.armsForwardLimitSwitchStatus);
+        this.logger.logBoolean(ClimberMechanism.logName, "armsReverseLimitSwitchStatus", this.armsReverseLimitSwitchStatus);
 
         this.logger.logNumber(ClimberMechanism.logName, "camVelocity", this.camVelocity);
         this.logger.logNumber(ClimberMechanism.logName, "camError", this.camError);
         this.logger.logNumber(ClimberMechanism.logName, "camPosition", this.camPosition);
+        this.logger.logBoolean(ClimberMechanism.logName, "camForwardLimitSwitchStatus", this.camForwardLimitSwitchStatus);
+        this.logger.logBoolean(ClimberMechanism.logName, "camReverseLimitSwitchStatus", this.camReverseLimitSwitchStatus);
 
     }
 
