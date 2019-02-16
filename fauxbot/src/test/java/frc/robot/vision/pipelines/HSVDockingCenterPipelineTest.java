78162e3be6bc7483725e7f9736c8ecaae1d92ddf
William Rall <William Rall>
2019-02-15 22:23:58 -0800

    Update unit tests for HSV Pipeline

--- fauxbot/src/test/java/frc/robot/vision/pipelines/HSVDockingCenterPipelineTest.java
+++ fauxbot/src/test/java/frc/robot/vision/pipelines/HSVDockingCenterPipelineTest.java
@@ -1,11 +1,31 @@
 package frc.robot.vision.pipelines;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class HSVDockingCenterPipelineTest {
+import org.junit.jupiter.api.Test;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.*;
+
+import frc.robot.common.robotprovider.*;
+
+public class HSVDockingCenterPipelineTest
+{
+    @Test
+    public void testLoadImage()
+    {
+        nu.pattern.OpenCV.loadShared();
+
+        Mat mat = Imgcodecs.imread("fauxbot/src/test/resources/frc.robot.vision.pipelines/Capture.PNG");
+        MatWrapper wrapper = new MatWrapper(mat);
+        HSVDockingCenterPipeline pipeline = new HSVDockingCenterPipeline(new FauxbotTimer(), new FauxbotProvider(), true);
+        pipeline.setActivation(true);
+        pipeline.process(wrapper);
+        assertNotNull(pipeline.getCenter());
+    }
 
     @Test
-    public void sampleTest() {
+    public void sampleTest()
+    {
 
     }
 
