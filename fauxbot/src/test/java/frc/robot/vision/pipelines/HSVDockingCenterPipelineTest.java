7f8173741437551a4312fd0567d262279c357799
James Violette <james.violette@violettesolutins.com>
2019-02-16 20:39:41 -0800

    Vision testing

--- fauxbot/src/test/java/frc/robot/vision/pipelines/HSVDockingCenterPipelineTest.java
+++ fauxbot/src/test/java/frc/robot/vision/pipelines/HSVDockingCenterPipelineTest.java
@@ -15,7 +15,9 @@ public class HSVDockingCenterPipelineTest
     {
         nu.pattern.OpenCV.loadShared();
 
-        Mat mat = Imgcodecs.imread("fauxbot/src/test/resources/frc.robot.vision.pipelines/Capture.PNG");
+        String imagePath = "C:\\Users\\james\\IdeaProjects2018\\irs1318_2019\\fauxbot\\src\\test\\resources\\frc.robot.vision.pipelines\\Capture.PNG";
+        // "fauxbot/src/test/resources/frc.robot.vision.pipelines/Capture.PNG"
+        Mat mat = Imgcodecs.imread(imagePath);
         MatWrapper wrapper = new MatWrapper(mat);
         HSVDockingCenterPipeline pipeline = new HSVDockingCenterPipeline(new FauxbotTimer(), new FauxbotProvider(), true);
         pipeline.setActivation(true);
