ab037e3d987e6a7d36cc3621bb6baf1f754c2210
DriverStation2 <DriverStation2@irs1318>
2019-02-17 16:20:35 -0800

    Add an option to process images for offboard vision

--- core/src/main/java/frc/robot/driver/Operation.java
+++ core/src/main/java/frc/robot/driver/Operation.java
@@ -6,6 +6,7 @@ public enum Operation
     VisionEnableCargoShip,
     VisionEnableRocket,
     VisionEnableOffboardStream,
+    VisionEnableOffboardProcessing,
 
     // DriveTrain operations:
     DriveTrainEnablePID,
