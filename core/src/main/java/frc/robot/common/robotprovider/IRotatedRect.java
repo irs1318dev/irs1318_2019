960b2f58a268c0ace7bb0fe4dcfa79d54f760843
James Violette <james.violette@violettesolutins.com>
2019-02-18 15:12:44 -0800

    Test vision

--- core/src/main/java/frc/robot/common/robotprovider/IRotatedRect.java
+++ core/src/main/java/frc/robot/common/robotprovider/IRotatedRect.java
@@ -2,9 +2,10 @@ package frc.robot.common.robotprovider;
 
 public interface IRotatedRect
 {
-    ISize size();
+    ISize getSize();
     IPoint getCenter();
     double getAngle();
+    double getAspectRatio();
     void set(double[] vals);
     IRect boundingRect();
     IRotatedRect clone();
--- core/src/main/java/frc/robot/vision/VisionCalculations.java
+++ core/src/main/java/frc/robot/vision/VisionCalculations.java
@@ -2,38 +2,39 @@ package frc.robot.vision;
 
 import frc.robot.GamePiece;
 import frc.robot.common.robotprovider.*;
-import frc.robot.vision.PixelsToInches;
-import frc.robot.vision.VisionConstants;
 import frc.robot.vision.common.VisionProcessingState;
+import frc.robot.vision.common.VisionResult;
 
 import java.util.*;
 
-public class VisionCalculations
-{
+public class VisionCalculations {
 
 
     public VisionCalculations() {
     }
 
-    public List<Set<IRotatedRect>> groupRotatedRect(List<IRotatedRect> rotatedRect)
-    {
+    public List<Set<IRotatedRect>> groupRotatedRect(List<IRotatedRect> rotatedRect) {
         List<Set<IRotatedRect>> rows = new ArrayList<>();
-        for(IRotatedRect rect : rotatedRect)
-        {
+        for (IRotatedRect rect : rotatedRect) {
             boolean added = false;
-            for(Set<IRotatedRect> row: rows)
-            {
+            // skip malformed rectangles
+            // reject rectangles that are malformed compared to the expected aspect ration of 5.5/2.0
+            if (rect.getAspectRatio() > 5.0 || rect.getAspectRatio() < 1.0) {
+                continue;
+            }
+            // put the rectangle in a row
+            for (Set<IRotatedRect> row : rows) {
                 IRotatedRect compare = row.stream().findFirst().get();
-                if(Math.abs(rect.getCenter().getY() - compare.getCenter().getY())< 30)
-                {
+                if (Math.abs(rect.getCenter().getY() - compare.getCenter().getY()) < 30) {
                     row.add(rect);
                     added = true;
                     break;
                 }
             }
-            if(added){
+            if (added) {
                 continue;
             }
+            // no row exists, create a new row
             Set<IRotatedRect> row = new HashSet<>();
             row.add(rect);
             rows.add(row);
@@ -41,205 +42,198 @@ public class VisionCalculations
         return rows;
     }
 
-    public Set<IRotatedRect> pickRow(List<Set<IRotatedRect>> rows, GamePiece gamePiece, VisionProcessingState processingState)
-    {
-        if(rows.size() == 1){
+    public VisionResult determineVisionResult(GamePiece gamePiece, VisionProcessingState processingState) {
+        if (GamePiece.Cargo.equals(gamePiece)
+                && VisionProcessingState.ActiveRocket.equals(processingState)) {
+            return VisionResult.HIGH_TARGET;
+        }
+        return VisionResult.LOW_TARGET;
+    }
+
+    public Set<IRotatedRect> pickRow(List<Set<IRotatedRect>> rows, VisionResult visionResult) {
+        if (rows.size() == 1) {
             return rows.get(0);
         }
-        if(GamePiece.Cargo.equals(gamePiece)
-        && VisionProcessingState.ActiveRocket.equals(processingState))
-        {
+        if (VisionResult.HIGH_TARGET.equals(visionResult)) {
             double lowestY = 1000;
             Set<IRotatedRect> lowestRow = null;
-            for(Set<IRotatedRect> row : rows)
-            {
+            for (Set<IRotatedRect> row : rows) {
                 IRotatedRect rect = row.stream().findFirst().get();
-                if(rect.getCenter().getY() < lowestY){
+                if (rect.getCenter().getY() < lowestY) {
                     lowestY = rect.getCenter().getY();
                     lowestRow = row;
                 }
-
             }
-           return lowestRow; 
-        }
-        else
-        {
+            return lowestRow;
+        } else {
             double highestY = 0;
             Set<IRotatedRect> highestRow = null;
-            for(Set<IRotatedRect> row : rows)
-            {
+            for (Set<IRotatedRect> row : rows) {
                 IRotatedRect rect = row.stream().findFirst().get();
-                if(rect.getCenter().getY() > highestY){
+                if (rect.getCenter().getY() > highestY) {
                     highestY = rect.getCenter().getY();
                     highestRow = row;
                 }
-
             }
             return highestRow;
         }
     }
 
     boolean isLeft(IRotatedRect rect) {
-        if(rect.getAngle() < -65 && rect.getAngle() > -85) {
+        if (rect.getAngle() < -65 && rect.getAngle() > -85) {
             return true;
         }
         return false;
     }
 
     boolean isRight(IRotatedRect rect) {
-        if(rect.getAngle() < 0  && rect.getAngle() > -15) {
+        if (rect.getAngle() < 0 && rect.getAngle() > -15) {
             return true;
         }
         return false;
     }
-    List<IRotatedRect> largestRect(List<IRotatedRect> rect)
-    {
+
+    List<IRotatedRect> largestRect(List<IRotatedRect> rect) {
         double area = 0;
         List<IRotatedRect> pair = new ArrayList<>();
         IRotatedRect left = null;
         IRotatedRect right = null;
-        for(int i = 0; i < rect.size(); i++)
-        {
-            if(isLeft(rect.get(i))){
-                if(area < (rect.get(i).size().getHeight() * rect.get(i).size().getWidth()))
-                {
-                    area = (rect.get(i).size().getHeight() * rect.get(i).size().getWidth());
+        for (int i = 0; i < rect.size(); i++) {
+            if (isLeft(rect.get(i))) {
+                if (area < (rect.get(i).getSize().getHeight() * rect.get(i).getSize().getWidth())) {
+                    area = (rect.get(i).getSize().getHeight() * rect.get(i).getSize().getWidth());
                     left = rect.get(i);
-                    right = rect.get(i+1);
+                    right = rect.get(i + 1);
+                    if (pair.isEmpty()) {
+                        pair.add(left);
+                        pair.add(right);
+                     } else {
+                        pair.set(0, left);
+                        pair.set(1, right);
+
+                    }
                 }
             }
         }
-        pair.add(left);
-        pair.add(right);
         return pair;
     }
 
-    public List<IRotatedRect> pickPairedRect(Set<IRotatedRect> rects)
-    {
+    public List<IRotatedRect> pickPairedRect(Set<IRotatedRect> rects) {
         List<IRotatedRect> rectList = new ArrayList<>();
         List<IRotatedRect> pairedRect = new ArrayList<>();
 
         rectList.addAll(rects);
-        Collections.sort(rectList, new Comparator<IRotatedRect>() {
+        Collections.sort(rectList,
+                Comparator.comparingDouble(arg0 -> arg0.getCenter().getX()));
 
-        @Override
-        public int compare(IRotatedRect arg0, IRotatedRect arg1) {
-            return Double.compare(arg0.getCenter().getX(), arg1.getCenter().getX());
-        }
-        });
-        for(int i = 0; i < rectList.size() - 1; i++)
-        {
-            if(isLeft(rectList.get(i)))
-            {
+        for (int i = 0; i < rectList.size(); i++) {
+            if (isLeft(rectList.get(i)) && i + 1 < rectList.size()) {
                 pairedRect.add(rectList.get(i));
-                pairedRect.add(rectList.get(i+1));
+                pairedRect.add(rectList.get(i + 1));
             }
         }
         return largestRect(pairedRect);
     }
 
-    public int findInterval(double avgPixelValue, List<PixelsToInches> interpolateList)
-    {
-        for(int i = 0; i < interpolateList.size(); i++)
-        {
+    public int findInterval(double avgPixelValue, List<PixelsToInches> interpolateList) {
+        for (int i = 0; i < interpolateList.size(); i++) {
             PixelsToInches pixelsToInches = VisionConstants.PIXELS_TO_INCHES.get(i);
-            if(avgPixelValue > pixelsToInches.getPixels())
-            {
+            if (avgPixelValue > pixelsToInches.getPixels()) {
                 return i - 1;
             }
         }
         return 0;
     }
 
-    public double computeAvgPixel(List<IRotatedRect> rects)
-    {
+    public double computeAvgPixel(List<IRotatedRect> rects) {
+        if (rects.size() != 2) {
+            return -1.0;
+        }
         IRotatedRect left = rects.get(0);
         IRotatedRect right = rects.get(1);
 
-        return ((left.size().getWidth() + right.size().getHeight())/ 2.0);
+        return ((left.getSize().getWidth() + right.getSize().getHeight()) / 2.0);
     }
 
-    public double interpolateInchesFromPixels(double avgPixel, int interval, List<PixelsToInches> interpolateList)
-    {
+    public double interpolateInchesFromPixels(double avgPixel, int interval, List<PixelsToInches> interpolateList) {
         PixelsToInches low = interpolateList.get(interval);
         PixelsToInches high = interpolateList.get(interval + 1);
-        double slope = (low.getInches()- high.getInches())/(low.getPixels() - high.getPixels());
+        double slope = (low.getInches() - high.getInches()) / (low.getPixels() - high.getPixels());
         double inches = slope * (avgPixel - low.getPixels()) + low.getInches();
         return inches;
     }
 
-    
-    public double azimuth(double avgPixels, List<IRotatedRect> list)
-    {
-        IRotatedRect left = list.get(0);
-        IRotatedRect right = list.get(1);
+
+    public double azimuth(double avgPixels, List<IRotatedRect> rects) {
+        if (rects.size() != 2) {
+            return -1000.0;
+        }
+        IRotatedRect left = rects.get(0);
+        IRotatedRect right = rects.get(1);
         //TODO: check for divide by zero
         double inchesPerPixels = VisionConstants.DOCKING_RETROREFLECTIVE_TAPE_HEIGHT_STRAIGHT / avgPixels;
         double observedPixels = (right.getCenter().getX() - left.getCenter().getX());
         double distanceBetweenCenters = VisionConstants.DOCKING_DISTANCE_BETWEEN_TAPE_TARGETS / inchesPerPixels;
-        double azimuth = Math.acos(observedPixels/ distanceBetweenCenters);
+        double azimuth = Math.acos(observedPixels / distanceBetweenCenters);
         return azimuth;
     }
 
-    public double calculateIntitialApproachTurnAngle(double azimuth, double pixelsPerInch, List<IRotatedRect> list, double distance)
-    {
-        IRotatedRect left = list.get(0);
-        IRotatedRect right = list.get(1);
+    public double calculateIntitialApproachTurnAngle(double azimuth, double pixelsPerInch, List<IRotatedRect> rects, double distance) {
+        if (rects.size() != 2) {
+            return -1000.0;
+        }
+        IRotatedRect left = rects.get(0);
+        IRotatedRect right = rects.get(1);
         //In radians
-        double targetCenter = VisionConstants.LIFECAM_CAMERA_RESOLUTION_X 
-                            + pixelsPerInch * VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET
-                            * Math.cos(azimuth);
+        double targetCenter = VisionConstants.LIFECAM_CAMERA_RESOLUTION_X
+                + pixelsPerInch * VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET
+                * Math.cos(azimuth);
 
-        double observedTargetCenter = left.getCenter().getX() 
-                                    + (right.getCenter().getX() -left.getCenter().getX()) / 2.0; 
-        double initialTurnAngle = (Math.atan(targetCenter - observedTargetCenter))/ distance;
+        double observedTargetCenter = left.getCenter().getX()
+                + (right.getCenter().getX() - left.getCenter().getX()) / 2.0;
+        double initialTurnAngle = (Math.atan(targetCenter - observedTargetCenter)) / distance;
         return initialTurnAngle;
     }
 
-    public double calculateTravelDistance(double azimuth, double distance)
-    {
+    public double calculateTravelDistance(double azimuth, double distance) {
         double travelDistance = Math.sqrt(
-            Math.pow(
-                (distance*Math.sin(azimuth) 
-                - VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET), 2) 
-            + Math.pow((distance*Math.cos(azimuth)), 2));
-            return travelDistance;
+                Math.pow(
+                        (distance * Math.sin(azimuth)
+                                - VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET), 2)
+                        + Math.pow((distance * Math.cos(azimuth)), 2));
+        return travelDistance;
     }
 
-    public double finalApproachTurn(double azimuth, double distance)
-    {
+    public double finalApproachTurn(double azimuth, double distance) {
         //24 is fill-in for docking offset
-        double approachAngle = Math.atan2(distance*Math.sin(azimuth)- 24, distance*Math.cos(azimuth));
+        double approachAngle = Math.atan2(distance * Math.sin(azimuth) - 24, distance * Math.cos(azimuth));
         //convert to radians!!!!!
-        return Math.PI/2 - approachAngle;
+        return Math.PI / 2 - approachAngle;
     }
 
-    public double glideRadius(double azimuth, double distance)
-    {
-        double radius = (Math.pow((distance * 
-                                Math.cos(azimuth) 
-                                - VisionConstants.DOCKING_CAMERA_MOUNTING_DISTANCE),2) 
-                        + Math.pow((distance * Math.sin(azimuth)
-                                - VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET), 2))
-                        /(2*distance*Math.sin(azimuth)
-                         + VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET);
+    public double glideRadius(double azimuth, double distance) {
+        double radius = (Math.pow((distance *
+                Math.cos(azimuth)
+                - VisionConstants.DOCKING_CAMERA_MOUNTING_DISTANCE), 2)
+                + Math.pow((distance * Math.sin(azimuth)
+                - VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET), 2))
+                / (2 * distance * Math.sin(azimuth)
+                + VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET);
 
         return radius;
     }
 
-    public double findSweepAngle(double distance, double azimuth, double radius)
-    {
-        double angle = (Math.atan(distance * Math.cos(azimuth) 
-                    - VisionConstants.DOCKING_CAMERA_MOUNTING_DISTANCE))
-                    / (radius - (distance * Math.sin(azimuth) 
-                    + VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET));
+    public double findSweepAngle(double distance, double azimuth, double radius) {
+        double angle = (Math.atan(distance * Math.cos(azimuth)
+                - VisionConstants.DOCKING_CAMERA_MOUNTING_DISTANCE))
+                / (radius - (distance * Math.sin(azimuth)
+                + VisionConstants.DOCKING_CAMERA_HORIZONTAL_MOUNTING_OFFSET));
 
         return angle;
     }
 
-    public double findGlideDistance(double radius, double angle)
-    {
-        return radius * angle; 
+    public double findGlideDistance(double radius, double angle) {
+        return radius * angle;
     }
-    
+
 }
