efde7c6dd01fb6714eb1b315dd439f79c8dfdb5a
William Rall <William Rall>
2019-02-19 11:33:47 -0800

    Switch Ring light PWM to channel 0

--- core/src/main/java/frc/robot/ElectronicsConstants.java
+++ core/src/main/java/frc/robot/ElectronicsConstants.java
@@ -32,7 +32,7 @@ public class ElectronicsConstants
 
     //================================================== Vision ==============================================================
 
-    public static final int VISION_RING_LIGHT_PWM_CHANNEL = 9;
+    public static final int VISION_RING_LIGHT_PWM_CHANNEL = 0;
 
     //================================================== Indicator Lights ==============================================================
 
