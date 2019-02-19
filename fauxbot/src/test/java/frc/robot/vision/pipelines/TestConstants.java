53243fca51bcb18177ab9772e4e63a13a2898f74
Driver Station A <issaquahroboticssociety@outlook.com>
2019-02-18 16:37:08 -0800

    Fix unit tests

--- fauxbot/src/test/java/frc/robot/vision/pipelines/TestConstants.java
+++ fauxbot/src/test/java/frc/robot/vision/pipelines/TestConstants.java
@@ -3,9 +3,9 @@ package frc.robot.vision.pipelines;
 public class TestConstants {
 
 
-    //public static final String BASE_PATH = "";
+    public static final String BASE_PATH = "";
     // use for local testing, in InteliJ, do not commit
-    public static final String BASE_PATH = "C:\\Users\\james\\IdeaProjects2018\\irs1318_2019\\";
+    //public static final String BASE_PATH = "C:\\Users\\james\\IdeaProjects2018\\irs1318_2019\\";
 
 
 }
