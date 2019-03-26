e6ac7a3e758a3d6666903331cf17388e9130e54b
Driver Station A <issaquahroboticssociety@outlook.com>
2019-03-25 20:07:22 -0700

    Update button for cargo load position on button pad to also go to cargo

--- core/src/main/java/frc/robot/driver/ButtonMap.java
+++ core/src/main/java/frc/robot/driver/ButtonMap.java
@@ -1326,7 +1326,9 @@ public class ButtonMap implements IButtonMap
                     UserInputDeviceButton.BUTTON_PAD_BUTTON_2,
                     Shift.Debug,
                     ButtonType.Toggle,
-                    () -> new ElevatorPositionTask(Operation.ElevatorCargoLoadPosition),
+                    () -> ConcurrentTask.AllTasks(
+                        new ElevatorPositionTask(Operation.ElevatorCargoLoadPosition),
+                        new GrabberSetWristPositionTask(Operation.GrabberWristCargoPosition)),
                     new Operation[]
                     {
                         Operation.ElevatorBottomPosition,
